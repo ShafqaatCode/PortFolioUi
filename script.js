f51d51023879a3be84e2f785d245e13e331ca2e7
ShafqaatCode <rajashajaatali@gmail.com>
2025-03-13 01:16:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+
+    // Toggle menu on hamburger click
+    hamburger.addEventListener('click', function () {
+        menu.classList.toggle('active');
+    });
+});
